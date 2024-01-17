001e7983de327aaf55d4186ee64cb1d1d803d4fe
herr kaste <herr.kaste@gmail.com>
2024-01-17 22:29:08 +0100

    Highlight var assignments

--- plugin.py
+++ plugin.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from itertools import chain
+from pathlib import Path
 import time
 import threading
 
@@ -10,6 +11,7 @@ from TreeSitter.src.api import get_node_spanning_region
 from TreeSitter.src import api
 
 flatten = chain.from_iterable
+PROJECT_ROOT = Path(__file__).parent
 
 
 @contextmanager
@@ -68,6 +70,7 @@ class CursorMoves(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
         highlight_callers(view)
         highlight_arguments(view)
+        highlight_vars(view)
 
 
 # @print_runtime("highlight_callers")
@@ -128,3 +131,56 @@ def highlight_arguments(view: sublime.View):
     contents = list(sublime.Region(left.b, right.a) for left, right in parens)
     view.add_regions('treesitter-args', contents, scope='pyhi-contents')
     view.add_regions('treesitter-parens', list(flatten(parens)), scope='pyhi-parens')
+
+
+@print_runtime("highlight_vars")
+def highlight_vars(view: sublime.View) -> None:
+    bid = view.buffer_id()
+    if not (tree_dict := api.get_tree_dict(bid)):
+        return None
+
+    queries_path = PROJECT_ROOT / "nvim-treesitter" / "queries"
+    query_file = "locals.scm"
+    scope = tree_dict["scope"]
+    node = tree_dict["tree"].root_node
+    captures = api.query_node(scope, node, query_file, queries_path)
+    scopes = [node for node, name in captures if name == "scope"]
+    # print(scopes)
+    definitions = [node for node, name in captures if name in ("definition.var", "definition.parameter")]
+    references = [
+        node for node, name in captures
+        if name == "reference"
+    ]
+    frozen_sel = [s for s in view.sel()]
+
+    refs = [
+        api.get_region_from_node(node_, view)
+        for s in frozen_sel
+        if (node := get_node_spanning_region(s, bid))
+        if (node in references)
+        if (node not in definitions)
+        if (ancestors := api.get_ancestors(node))
+        if (containing_scopes := sorted(
+            (scope for scope in scopes if api.contains(scope, node)),
+            key=api.get_size,
+        ))
+        if (local_references := next((
+            local_refs
+            for cs in containing_scopes
+            if (local_refs := [
+                ref for ref in definitions
+                if ref.text == node.text
+                if api.contains(cs, ref)
+                if (defining_scope := min(
+                    (scope for scope in scopes if api.contains(scope, ref)),
+                    key=api.get_size,
+                    default=None
+                ))
+                if defining_scope in ancestors
+            ])
+            # if (print(local_refs) or True)
+        ), []))
+        for node_ in local_references
+    ]
+
+    view.add_regions('treesitter-refs', refs, scope='pyhi-refs')
